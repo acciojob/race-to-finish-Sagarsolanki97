c462e5010a3f50f2ba74a32c4f532d72ac7f3231
SAGAR SOLANKI <118007645+Sagarsolanki97@users.noreply.github.com>
2023-04-28 12:05:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-window.promises = [];
+// Array of 5 promises that resolve with a random time between 1 and 5 seconds
+const promises = [
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 4000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 4000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 4000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 4000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(Math.random() * 4 + 1), Math.random() * 4000 + 1000))
+];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+// Wait for the first promise to resolve using Promise.any()
+Promise.any(promises)
+  .then(result => {
+    const output = document.getElementById('output');
+    output.innerText = `The first promise to resolve has returned ${result.toFixed(2)} seconds.`;
+  })
+  .catch(error => console.error(error));
